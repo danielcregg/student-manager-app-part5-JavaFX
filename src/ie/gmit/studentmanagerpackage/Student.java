97529c7b2bb0e07c54464c645a221bfe0f35797e
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-10-31 21:54:48 +0000

    Initial commit

---
+++ src/ie/gmit/studentmanagerpackage/Student.java
@@ -0,0 +1,68 @@
+package ie.gmit.studentmanagerpackage;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	// Instance Variables
+	private String studentId;
+	private String fisrtName;
+	private String surname;
+	private int yearOfStudy;
+
+	// Constructor1
+	public Student(String studentId) {
+		this.studentId = studentId;
+	}
+	
+	// Constructor2
+	public Student(String studentId, String fisrtName, String surname) {
+		this.studentId = studentId;
+		this.fisrtName = fisrtName;
+		this.surname = surname;
+	}
+
+	// Constructor3
+	public Student(String studentId, String fisrtName, String surname, int yearOfStudy) {
+		this.studentId = studentId;
+		this.fisrtName = fisrtName;
+		this.surname = surname;
+		this.yearOfStudy = yearOfStudy;
+	}
+
+	// Getters and Setters
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFisrtName() {
+		return fisrtName;
+	}
+
+	public void setFisrtName(String fisrtName) {
+		this.fisrtName = fisrtName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getYearOfStudy() {
+		return yearOfStudy;
+	}
+
+	public void setYearOfStudy(int yearOfStudy) {
+		this.yearOfStudy = yearOfStudy;
+	}
+
+}
